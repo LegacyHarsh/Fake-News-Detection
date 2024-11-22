26ef65a73329e111c988c3cdd3c98bfe0baa73d1
Harshit Chaudhary <140608411+LegacyHarsh@users.noreply.github.com>
2024-11-22 08:42:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import pickle
 
 app = Flask(__name__)
 
-# Load the model and vectorizer
 with open("gradient_boosting_model.pkl", "rb") as model_file:
     model = pickle.load(model_file)
 
@@ -12,20 +11,16 @@ with open("vectorizer.pkl", "rb") as vectorizer_file:
 
 @app.route('/')
 def home():
-    return render_template('index.html')  # Renders an HTML form
+    return render_template('index.html')  
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get the news content from the form
     news_content = request.form['news']
     
-    # Transform the input text using the vectorizer
     transformed_input = vectorizer.transform([news_content])
     
-    # Make a prediction
     prediction = model.predict(transformed_input)[0]
     
-    # Return the result
     result = "Fake News" if prediction == 1 else "Real News"
     return jsonify({'prediction': result})
 
